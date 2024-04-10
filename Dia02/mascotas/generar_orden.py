e2b16d39bff81fcc41b7dc08db84d4f4dbc1d8d2
Najla Gatica <najla.gatica@gmail.com>
2024-04-09 20:29:38 -0400

    Ejercicio Guiado: Tienda de mascotas v1

---
+++ Dia02/mascotas/generar_orden.py
@@ -0,0 +1,28 @@
+# archivo generar_orden.py
+from orden_compra import OrdenCompra
+
+#crear objeto de tipo OrdenCompra, y llamar al método nueva_orden
+oc = OrdenCompra()
+oc.nueva_orden()
+
+#solicitar al usuario el valor para el identificador
+oc.identificador = int(input("Ingrese identificador de la OC:\n"))
+
+#total de productos y monto
+oc.total_productos = int(input("Ingrese total de productos:\n"))
+oc.monto = int(input("Ingrese monto:\n"))
+
+#Determinar si se aplica codigo de descuento. EL programa lo detecta según lo que usuario ingresó
+if oc.monto > 20000:
+    oc.codigo_descuento = "20PORCIENTO"
+elif oc.monto > 10000:
+    oc.codigo_descuento = "10PORCIENTO"
+
+
+
+""" Ingrese identificador de la OC:
+112233
+Ingrese total de productos:
+100
+Ingrese monto:
+5000"""
---
+++ Dia02/mascotas/orden_compra.py
@@ -0,0 +1,13 @@
+# archivo orden_compra.py
+class OrdenCompra():
+
+# Se crea un método de instancia para definir atributos
+    def nueva_orden(self):
+
+        # Se define los atributos de instancia
+        self.identificador = 0
+        self.total_productos = 0
+        self.monto = 0
+        self.codigo_descuento = ""
+
+
