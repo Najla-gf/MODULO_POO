48f3f3a677c9fcbcba65c3290c0640a76c5d3af7
Najla Gatica <najla.gatica@gmail.com>
2024-04-17 10:51:10 -0400

    Desafio guiado grupo 2 en clases

--- Dia08/DG_membresia/membresia2.py
+++ Dia08/DG_membresia/membresia2.py
@@ -1,23 +1,8 @@
 from abc import ABC, abstractmethod
 
-class TodasLasMembresias:
-    def crear_membresia(self, tipo_membresia, correo_electronico, numero_tarjeta):
-        # Crear membresía según el tipo especificado
-        if tipo_membresia == 1:
-            return MembresiaBasica(correo_electronico, numero_tarjeta)
-        elif tipo_membresia == 2:
-            return MembresiaFamiliar(correo_electronico, numero_tarjeta)
-        elif tipo_membresia == 3:
-            return MembresiaSinConexion(correo_electronico, numero_tarjeta)
-        elif tipo_membresia == 4:
-            return MembresiaPro(correo_electronico, numero_tarjeta)
-        else:
-            return MembresiaGratis(correo_electronico, numero_tarjeta)
-
-
 class Membresia(ABC):
-    def __init__(self, correo_electronico, numero_tarjeta):
-        self.__correo_electronico = correo_electronico
+    def __init__(self, correo_suscriptor: str, numero_tarjeta: str):
+        self.__correo_suscriptor = correo_suscriptor
         self.__numero_tarjeta = numero_tarjeta
 
     @abstractmethod
@@ -25,161 +10,82 @@ class Membresia(ABC):
         pass
 
     @abstractmethod
-    def cambiar_membresia(self, tipo_membresia):
+    def cambiar_suscripcion(self, tipo_membresia):
         pass
 
-    @property
-    def correo_electronico(self):
-        return self.__correo_electronico
-
-    @property
-    def numero_tarjeta(self):
-        return self.__numero_tarjeta
-
-class MembresiaGratis(Membresia):
-    costo = 0
-    dispositivos_maximos = 1
-
+class Gratis(Membresia):
     def cancelar_suscripcion(self):
-        return MembresiaGratis(self.correo_electronico, self.numero_tarjeta)
+        return Gratis(super().__correo_suscriptor, super().__numero_tarjeta)
 
-    def cambiar_membresia(self, tipo_membresia):
-        todas_las_membresias = TodasLasMembresias()
-        return todas_las_membresias.crear_membresia(tipo_membresia, self.correo_electronico, self.numero_tarjeta)
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia == 1:
+            return Basica(super().__correo_suscriptor, super().__numero_tarjeta)
+        elif tipo_membresia == 2:
+            return Familiar(super().__correo_suscriptor, super().__numero_tarjeta)
+        elif tipo_membresia == 3:
+            return SinConexion(super().__correo_suscriptor, super().__numero_tarjeta)
+        elif tipo_membresia == 4:
+            return Pro(super().__correo_suscriptor, super().__numero_tarjeta)
+        else:
+            return Gratis(super().__correo_suscriptor, super().__numero_tarjeta)
 
-class MembresiaBasica(Membresia):
-    costo = 3000
-    dispositivos_maximos = 2
-
+class Basica(Membresia):
     def cancelar_suscripcion(self):
-        return MembresiaGratis(self.correo_electronico, self.numero_tarjeta)
+        return Gratis(super().__correo_suscriptor, super().__numero_tarjeta)
 
-    def cambiar_membresia(self, tipo_membresia):
-        todas_las_membresias = TodasLasMembresias()
-        return todas_las_membresias.crear_membresia(tipo_membresia, self.correo_electronico, self.numero_tarjeta)
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia >= 2 and tipo_membresia <= 4:
+            if tipo_membresia == 2:
+                return Familiar(super().__correo_suscriptor, super().__numero_tarjeta)
+            elif tipo_membresia == 3:
+                return SinConexion(super().__correo_suscriptor, super().__numero_tarjeta)
+            elif tipo_membresia == 4:
+                return Pro(super().__correo_suscriptor, super().__numero_tarjeta)
+        else:
+            return self
 
-class MembresiaFamiliar(Membresia):
-    costo = 5000
-    dispositivos_maximos = 5
-
-    def __init__(self, correo_electronico, numero_tarjeta):
-        super().__init__(correo_electronico, numero_tarjeta)
-        self.__dias_regalo = 7 if isinstance(self, MembresiaFamiliar) else 0
-        self.__control_parental = False
-
-    @property
-    def dias_regalo(self):
-        return self.__dias_regalo
-
-    @property
-    def control_parental(self):
-        return self.__control_parental
-
+class Familiar(Basica):
     def cancelar_suscripcion(self):
-        return MembresiaGratis(self.correo_electronico, self.numero_tarjeta)
+        return Gratis(super().__correo_suscriptor, super().__numero_tarjeta)
 
-    def cambiar_membresia(self, tipo_membresia):
-        todas_las_membresias = TodasLasMembresias()
-        return todas_las_membresias.crear_membresia(tipo_membresia, self.correo_electronico, self.numero_tarjeta)
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia in [1, 3, 4]:
+            return super().cambiar_suscripcion(tipo_membresia)
+        else:
+            return self
 
-    def modificar_control_parental(self, activado):
-        self.__control_parental = activado
-
-
-class MembresiaSinConexion(Membresia):
+class SinConexion(Basica):
     costo = 3500
-    dispositivos_maximos = 2
-
-    def __init__(self, correo_electronico, numero_tarjeta):
-        super().__init__(correo_electronico, numero_tarjeta)
-        self.__dias_regalo = 7 if isinstance(self, MembresiaSinConexion) else 0
-        self.__contenido_sin_conexion = 0
-
-    @property
-    def dias_regalo(self):
-        return self.__dias_regalo
-
-    @property
-    def contenido_sin_conexion(self):
-        return self.__contenido_sin_conexion
 
     def cancelar_suscripcion(self):
-        return MembresiaGratis(self.correo_electronico, self.numero_tarjeta)
+        return Gratis(super().__correo_suscriptor, super().__numero_tarjeta)
 
-    def cambiar_membresia(self, tipo_membresia):
-        todas_las_membresias = TodasLasMembresias()
-        return todas_las_membresias.crear_membresia(tipo_membresia, self.correo_electronico, self.numero_tarjeta)
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia in [1, 2, 4]:
+            return super().cambiar_suscripcion(tipo_membresia)
+        else:
+            return self
 
-    def incrementar_contenido_sin_conexion(self, cantidad):
-        self.__contenido_sin_conexion += cantidad
-
-
-class MembresiaPro(Membresia):
-    costo = 7000
-    dispositivos_maximos = 6
-
-    def __init__(self, correo_electronico, numero_tarjeta):
-        super().__init__(correo_electronico, numero_tarjeta)
-        self.__dias_regalo = 15 if isinstance(self, MembresiaPro) else 0
-        self.__control_parental = False
-
-    @property
-    def dias_regalo(self):
-        return self.__dias_regalo
-
-    @property
-    def control_parental(self):
-        return self.__control_parental
-
+class Pro(Familiar, SinConexion):
     def cancelar_suscripcion(self):
-        return MembresiaGratis(self.correo_electronico, self.numero_tarjeta)
+        return Gratis(super().__correo_suscriptor, super().__numero_tarjeta)
 
-    def cambiar_membresia(self, tipo_membresia):
-        todas_las_membresias = TodasLasMembresias()
-        return todas_las_membresias.crear_membresia(tipo_membresia, self.correo_electronico, self.numero_tarjeta)
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia in [1, 2, 3]:
+            return super().cambiar_suscripcion(tipo_membresia)
+        else:
+            return self
 
-#################
-#EJEMPLO DE USO
 
-# Crear una membresía básica
-membresia_actual = MembresiaBasica("correo@example.com", "1234567890")
-print("Membresía actual: Básica")
-
-# Cambiar a una membresía familiar
-membresia_actual = membresia_actual.cambiar_membresia(2)
-print("Cambiando a: Familiar")
-print("Membresía actual: Familiar")
-
-# Verificar días de regalo y control parental (solo disponible para membresía familiar)
-if isinstance(membresia_actual, MembresiaFamiliar):
-    print("Días de regalo:", membresia_actual.dias_regalo)
-    print("Control parental:", membresia_actual.control_parental)
-
-# Cambiar a una membresía sin conexión
-membresia_actual = membresia_actual.cambiar_membresia(3)
-print("Cambiando a: Sin Conexión")
-print("Membresía actual: Sin Conexión")
-
-# Incrementar el contenido sin conexión (solo disponible para membresía sin conexión)
-if isinstance(membresia_actual, MembresiaSinConexion):
-    membresia_actual.incrementar_contenido_sin_conexion(10)
-    print("Contenido sin conexión:", membresia_actual.contenido_sin_conexion)
-
-# Cambiar a una membresía pro
-membresia_actual = membresia_actual.cambiar_membresia(4)
-print("Cambiando a: Pro")
-print("Membresía actual: Pro")
-
-# Verificar días de regalo y control parental (solo disponible para membresía pro)
-if isinstance(membresia_actual, MembresiaPro):
-    print("Días de regalo:", membresia_actual.dias_regalo)
-    print("Control parental:", membresia_actual.control_parental)
-
-# Cancelar la suscripción
-membresia_actual = membresia_actual.cancelar_suscripcion()
-print("Cancelando suscripción")
-print("Membresía actual: Gratis")
-
-# Verificar los atributos de la membresía actual
-print("Correo electrónico:", membresia_actual.correo_electronico)
-print("Número de tarjeta:", membresia_actual.numero_tarjeta)
+g = Gratis("correo@prueba.cl", "123 456 789")
+print(type(g))
+b = g.cambiar_suscripcion(1)
+print(type(b))
+f = b.cambiar_suscripcion(2)
+print(type(f))
+sc = f.cambiar_suscripcion(3)
+print(type(sc))
+pro = sc.cambiar_suscripcion(4)
+print(type(pro))
+g2 = pro.cancelar_suscripcion()
+print(type(g2))
---
+++ Dia08/DG_membresia/membresia_enclase.py
@@ -0,0 +1,108 @@
+from abc import ABC, abstractmethod
+
+class Membresia(ABC):
+    def __init__(self, correo_suscriptor: str, numero_tarjeta: str):
+        self.__correo_suscriptor = correo_suscriptor #__ metodo privado, no se pueden sobreescribir
+        self.__numero_tarjeta = numero_tarjeta #_ metodo oculto, se puede sobreescribir
+
+    @property
+    def correo_suscriptor(self): #Getter para el correo de la membresía
+        return self.__correo_suscriptor
+
+    @property
+    def numero_tarjeta(self): #Getter para la tarjeta asociada a la membresía
+        return self.__numero_tarjeta    
+
+    @abstractmethod
+    def cancelar_suscripcion(self):
+        return Gratis(self.correo_suscriptor, self.numero_tarjeta)
+
+    @abstractmethod
+    def cambiar_suscripcion(self, tipo_membresia):
+        pass
+
+    def _crear_nueva_membresia(self, tipo_membresia):
+        if tipo_membresia == 1:
+            return Basica(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo_membresia == 2:
+            return Familiar(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo_membresia == 3:
+            return SinConexion(self.correo_suscriptor, self.numero_tarjeta)
+        elif tipo_membresia == 4:
+            return Pro(self.correo_suscriptor, self.numero_tarjeta)
+
+
+class Gratis(Membresia):
+    costo = 0
+    dispositivos_maximos = 1
+    
+    def cambiar_suscripcion(self, tipo_membresia):
+        return self._crear_nueva_membresia(tipo_membresia)
+
+class Basica(Membresia):
+    costo = 3000
+    dispositivos_maximos = 2
+    
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia >= 2 and tipo_membresia <= 4:
+            return self._crear_nueva_membresia(tipo_membresia)
+        return self #si no selecciona ninguna, se mantiene en básica
+
+class Familiar(Basica):
+    costo = 5000
+    dispositivos_maximos = 5
+    dias_regalo = 7
+
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia in [1,3,4]:
+            return self._crear_nueva_membresia(tipo_membresia)
+        return self 
+
+    def control_parental(self):
+        pass
+
+
+class SinConexion(Basica):
+    costo = 3500
+    dispositivos_maximos = 2
+    dias_regalo = 7
+    
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia in [1,2,4]:
+            return self._crear_nueva_membresia(tipo_membresia)
+        return self 
+
+    def incrementar_contenido(self):
+        pass
+
+
+class Pro(Familiar, SinConexion):
+    costo = 7000
+    dispositivos_maximos = 6
+    dias_regalo = 15
+
+    def cambiar_suscripcion(self, tipo_membresia):
+        if tipo_membresia >= 1 and tipo_membresia <= 3:
+            return self._crear_nueva_membresia(tipo_membresia)
+        return self 
+    
+    def control_parental(self):
+        pass
+    
+    def incrementar_contenido(self):
+        pass
+
+
+
+g = Gratis("correo@prueba.cl", "123 456 789")
+print(type(g))
+b = g.cambiar_suscripcion(1)
+print(type(b))
+f = b.cambiar_suscripcion(2)
+print(type(f))
+sc = f.cambiar_suscripcion(3)
+print(type(sc))
+pro = sc.cambiar_suscripcion(4)
+print(type(pro))
+g2 = pro.cancelar_suscripcion()
+print(type(g2))
